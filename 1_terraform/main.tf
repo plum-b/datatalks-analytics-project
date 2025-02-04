3f3dc41424c861c4794da8dd0c59bc99085c8af2
Afif Nahas <afifnahas1998@gmail.com>
2025-02-04 02:16:49 +0200

    Updated terraform

--- 1_terraform/main.tf
+++ 1_terraform/main.tf
@@ -2,23 +2,104 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.18.1"
+      version = "~> 6.18"
     }
   }
 }
 
 provider "google" {
-  credentials = file(var.credentials_file)
-  project     = var.project_id
-  region      = var.region
+  project = var.project_id
+  region  = var.region
 }
 
-resource "google_storage_bucket" "raw_bucket" {
-  name     = var.bucket_raw_name
-  location = "US"
+# Storage Buckets
+resource "google_storage_bucket" "raw" {
+  name          = "ny-taxi-raw"
+  location      = "US"
+  storage_class = "STANDARD"
 }
 
-resource "google_storage_bucket" "processed_bucket" {
-  name     = var.bucket_processed_name
-  location = "US"
+resource "google_storage_bucket" "processed" {
+  name          = "ny-taxi-processed"
+  location      = "US"
+  storage_class = "STANDARD"
+}
+
+# BigQuery Dataset
+resource "google_bigquery_dataset" "ny_taxi" {
+  dataset_id    = "ny_taxi_dataset"
+  friendly_name = "NY Taxi Dataset"
+  location      = "US"
+}
+
+# BigQuery Table
+resource "google_bigquery_table" "ny_taxi" {
+  dataset_id          = google_bigquery_dataset.ny_taxi.dataset_id
+  table_id            = "ny_taxi"
+  schema              = file("${path.module}/schema/ny_taxi_schema.json")
+  deletion_protection = false
+}
+
+# Dataproc Cluster (Updated)
+resource "google_dataproc_cluster" "ny_taxi_processing" {
+  name   = "ny-taxi-processing"
+  region = var.region
+
+  cluster_config {
+    master_config {
+      num_instances = 1
+      machine_type  = "n1-standard-2"
+    }
+
+    worker_config {
+      num_instances = 2  # Initial number of instances
+      machine_type  = "n1-standard-2"
+    }
+
+    autoscaling_config {
+      policy_uri = google_dataproc_autoscaling_policy.basic.self_link
+    }
+
+    software_config {
+      image_version = "2.0"
+      optional_components = ["JUPYTER"]
+    }
+  }
+}
+
+# Autoscaling Policy (Updated)
+resource "google_dataproc_autoscaling_policy" "basic" {
+  policy_id = "ny-taxi-autoscaling"
+  location  = var.region
+
+  worker_config {
+    min_instances = 2
+    max_instances = 10  # Correct attribute name
+  }
+
+  basic_algorithm {
+    yarn_config {
+      graceful_decommission_timeout = "30s"
+      scale_up_factor   = 0.05
+      scale_down_factor = 0.05
+    }
+  }
+}
+
+resource "google_dataproc_autoscaling_policy" "basic" {
+  policy_id = "ny-taxi-autoscaling"
+  location  = var.region
+
+  worker_config {
+    min_instances = 2
+    max_instances = 10
+  }
+
+  basic_algorithm {
+    yarn_config {
+      graceful_decommission_timeout = "30s"
+      scale_up_factor   = 0.05
+      scale_down_factor = 0.05
+    }
+  }
 }
--- 1_terraform/variables.tf
+++ 1_terraform/variables.tf
@@ -1,37 +1,16 @@
-variable "credentials_file" {
-  type        = string
-  description = "Path to the GCP service account credentials JSON."
-}
-
 variable "project_id" {
+  description = "The GCP Project ID"
   type        = string
-  description = "Google Cloud project ID."
 }
 
 variable "region" {
+  description = "The GCP region"
   type        = string
-  description = "Region for resources."
   default     = "us-central1"
 }
 
-variable "bucket_raw_name" {
+variable "zone" {
+  description = "The GCP zone"
   type        = string
-  description = "Unique name for raw GCS bucket."
-}
-
-variable "bucket_processed_name" {
-  type        = string
-  description = "Unique name for processed GCS bucket."
-}
-
-variable "dataset_id" {
-  type        = string
-  description = "BigQuery dataset ID."
-  default     = "ny_taxi_data"
-}
-
-variable "service_account_id" {
-  type        = string
-  description = "Service account ID (without domain)."
-  default     = "dataproc-spark-sa"
+  default     = "us-central1-a"
 }
